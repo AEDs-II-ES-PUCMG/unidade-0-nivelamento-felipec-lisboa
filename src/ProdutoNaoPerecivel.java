d30f424fba46fa44c2fe39d1fa7ab4cd48f4c616
felipec-lisboa <lilfelipecosta@gmail.com>
2026-03-02 22:22:58 -0300

    Aula do dia 02/03/2026

--- src/ProdutoNaoPerecivel.java
+++ src/ProdutoNaoPerecivel.java
@@ -7,6 +7,13 @@ public class ProdutoNaoPerecivel extends Produto{
     public ProdutoNaoPerecivel(String desc, double precoCusto){
         super(desc, precoCusto);
     }
+
+    @Override
+            public String gerarDadosTexto(){
+                String precoFormatado = String.format("%.2f",precoCusto).replace(",", ".");
+                String margemFormatada = String.format("%2.f",margemLucro).replace(",",".");
+                return String.format("1;%s;%s;%s", descricao,precoFormatado,margemFormatada);
+            }
     
 
 }
